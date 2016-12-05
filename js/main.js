74163f263e075378c36d749d8ac79691c4e51e9c
MaryoryChavez <maryory.chavez@gmail.com>
2016-12-05 17:40:06 -0500

    version phone listo

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*--------------Menu----------*/
+var altura = $('header').offset().top;
+
+$(window).on('scroll', function(){
+	if ( $(window).scrollTop() > altura ){
+		$('header').addClass('menu-fixed');
+	} else {
+		$('header').removeClass('menu-fixed');
+	}
+});
+
+
+/*--------------Desplasamiento opciones----------*/
+$(function(){
+
+     $('a[href*=#]').click(function() {
+
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+         && location.hostname == this.hostname) {
+
+             var $target = $(this.hash);
+
+             $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+
+             if ($target.length) {
+
+                 var targetOffset = $target.offset().top;
+
+                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+
+                 return false;
+
+            }
+
+       }
+
+   });
+
+});
